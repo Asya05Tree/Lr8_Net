9690e4a810df0ebb938b19be3c1ed9a56fe76f9b
Nasya_K <71969033+Asya05Tree@users.noreply.github.com>
2024-10-22 10:49:02 +0300

    Add files via upload

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using Lr8.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lr8.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public ProductController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // GET: /Product
+        public IActionResult List()
+        {
+            var products = _configuration.GetSection("Products").Get<List<Product>>();
+            return View(products);
+        }
+
+        // GET: /Product/Details/5
+        public IActionResult Details(int id)
+        {
+            var products = _configuration.GetSection("Products").Get<List<Product>>();
+            var product = products?.FirstOrDefault(p => p.ID == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lr8.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Назва товару обов'язкова")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Довжина назви має бути від 3 до 100 символів")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Ціна обов'язкова")]
+        [Range(0.01, 10000, ErrorMessage = "Ціна має бути від 0.01 до 10000")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Дата створення обов'язкова")]
+        [DataType(DataType.Date)]
+        public DateTime CreatedDate { get; set; }
+    }
+}
